5bbea66a33866ea0d25aacf620ea172cebb81443
Matteo-x220 <matteosasoris@gmail.com>
2020-09-15 00:42:36 +0200

    Changed Font

--- src/pages/Writer.js
+++ src/pages/Writer.js
@@ -1,6 +1,6 @@
-import React, { Component } from "react"
-import Container from 'react-bootstrap/Container'
-import axios from "axios"
+import React, { Component } from "react";
+import Container from "react-bootstrap/Container";
+import axios from "axios";
 
 /*  Ok, I'm being serious here. WHY THE FUCK DO YOU PUT 
     THE FUCKING DOLLAR SIGN ($) IN YOUR FUCKING SHITTY API
@@ -19,29 +19,29 @@ import axios from "axios"
     Thanks god there's alcohol.
 */
 
-const { REACT_APP_JSONBinURI, REACT_APP_JSONBinAPIKEY } = process.env
+const { REACT_APP_JSONBinURI, REACT_APP_JSONBinAPIKEY } = process.env;
 
 export default class Writer extends Component {
   handleTitleChange = (event) => {
-    this.setState({ title: event.target.value })
-  }
+    this.setState({ title: event.target.value });
+  };
   handleContentChange = (event) => {
-    this.setState({ content: event.target.value })
-  }
+    this.setState({ content: event.target.value });
+  };
   handleSubmit = (event) => {
     let article = {
       title: this.state.title,
       content: this.state.content,
       id: new Date().getTime(),
-    }
+    };
     axios
       .get(REACT_APP_JSONBinURI + "/latest", {
         headers: { "secret-key": REACT_APP_JSONBinAPIKEY },
       })
       .then((response) => {
-        let articlesList = response.data.articles
-        console.log("Lista articoli:", articlesList)
-        articlesList.push(article)
+        let articlesList = response.data.articles;
+        console.log("Lista articoli:", articlesList);
+        articlesList.push(article);
         axios
           .put(
             REACT_APP_JSONBinURI,
@@ -54,22 +54,22 @@ export default class Writer extends Component {
             }
           )
           .then(() => {
-            alert("articolo mandato")
+            alert("articolo mandato");
           })
           .catch((error) => {
-            console.log(error)
-          })
+            console.log(error);
+          });
       })
       .catch((error) => {
-        console.log(error)
-      })
-    event.preventDefault()
-  }
+        console.log(error);
+      });
+    event.preventDefault();
+  };
 
   render() {
     return (
       <Container>
-        <form className="border border-light p-5" onSubmit={this.handleSubmit}>
+        <form className="p-5" onSubmit={this.handleSubmit}>
           <p className="h4 mb-4 text-left">Submit an article</p>
           <input
             type="text"
